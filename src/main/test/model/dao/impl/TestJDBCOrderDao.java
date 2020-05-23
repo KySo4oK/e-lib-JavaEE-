6cd6ea87e951d4f72c9112c608cab013bd0c6d65
KySo4oK <prostorostuk08@gmail.com>
2020-05-23 23:12:52 +0300

    tested create, update and delete order

--- src/main/test/model/dao/impl/TestJDBCOrderDao.java
+++ src/main/test/model/dao/impl/TestJDBCOrderDao.java
@@ -40,15 +40,32 @@ public class TestJDBCOrderDao {
             Assert.assertTrue(orderList.contains(order));
         }
     }
+
     @Test
-    public void testCreate(){
+    public void testCreate() {
         orderDao.create(Order.Builder.anOrder().active(true).book(
                 DaoFactory.getInstance().createBookDao().findAll().get(0))
                 .user(DaoFactory.getInstance().createUserDao().findAll().get(0)).build());
     }
 
     @Test
-    public void testDelete(){
-        orderDao.delete(40);
+    public void testUpdate() {
+        Order order = orderDao.findAll().get(0);
+        boolean before = order.isActive();
+        order.setActive(!before);
+        orderDao.update(order);
+        boolean after = orderDao.findById(order.getOrderId()).get().isActive();
+        if (before == after) {
+            Assert.fail();
+        }
+    }
+
+    @Test
+    public void testDelete() {
+        try {
+            orderDao.delete(orderDao.findAll().stream().map(Order::getOrderId).max(Long::compareTo).get());
+        } catch (Exception e) {
+            Assert.fail();
+        }
     }
 }
