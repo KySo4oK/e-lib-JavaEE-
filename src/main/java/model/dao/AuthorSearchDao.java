d3d39c8d71418193da065028cbda402a44b57d40
KySo4oK <prostorostuk08@gmail.com>
2020-06-16 20:53:18 +0300

    implement part of author search dao methods

--- src/main/java/model/dao/AuthorSearchDao.java
+++ src/main/java/model/dao/AuthorSearchDao.java
@@ -3,4 +3,7 @@ package model.dao;
 import model.entity.AuthorSearch;
 
 public interface AuthorSearchDao extends GenericDao<AuthorSearch> {
+    String SQL_DELETE = "delete from author_search where author_search_id = ?";
+    String SQL_UPDATE = "update author_search set search_date = ? where author_search_id = ?";
+    String SQL_INSERT = "insert into author_search (user_id, author_id) values (?, ?)";
 }
--- src/main/java/model/dao/impl/JDBCAuthorSearchDao.java
+++ src/main/java/model/dao/impl/JDBCAuthorSearchDao.java
@@ -1,20 +1,38 @@
 package model.dao.impl;
 
 import model.dao.AuthorSearchDao;
+import model.dao.mapper.impl.AuthorSearchMapper;
+import model.entity.Author;
 import model.entity.AuthorSearch;
 
 import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
 public class JDBCAuthorSearchDao implements AuthorSearchDao {
-    public JDBCAuthorSearchDao(Connection connection) {
+    private final Connection connection;
+    private final AuthorSearchMapper mapper = new AuthorSearchMapper();
 
+    public JDBCAuthorSearchDao(Connection connection) {
+        this.connection = connection;
     }
 
     @Override
     public void create(AuthorSearch entity) {
-
+        Long userId = entity.getUser().getId();
+        try (PreparedStatement statement = connection.prepareStatement(SQL_INSERT)) {
+            for (Author author : entity.getAuthors()) {
+                statement.setLong(1, userId);
+                statement.setLong(2, author.getAuthorId());
+                statement.addBatch();
+            }
+            statement.executeBatch();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -29,16 +47,31 @@ public class JDBCAuthorSearchDao implements AuthorSearchDao {
 
     @Override
     public void update(AuthorSearch entity) {
-
+        try (PreparedStatement statement = connection.prepareStatement(SQL_UPDATE)) {
+            statement.setDate(1, Date.valueOf(entity.getSearchDate()));
+            statement.setLong(2, entity.getAuthorSearchId());
+            statement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void delete(long id) {
-
+        try (PreparedStatement statement = connection.prepareStatement(SQL_DELETE)) {
+            statement.setLong(1, id);
+            statement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void close() {
-
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
