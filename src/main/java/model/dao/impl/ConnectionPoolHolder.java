230d7121aa0a2985a35146b18f0614b040bdce5b
KySo4oK <prostorostuk08@gmail.com>
2020-06-28 12:07:29 +0300

    move database properties to file in resources

--- src/main/java/model/dao/impl/ConnectionPoolHolder.java
+++ src/main/java/model/dao/impl/ConnectionPoolHolder.java
@@ -1,25 +1,40 @@
 package model.dao.impl;
 
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import javax.sql.DataSource;
+import java.io.FileReader;
+import java.util.Properties;
 
 public class ConnectionPoolHolder {
+    private static final String DB_PROPERTIES =
+            "C:\\Users\\KySo4oK\\IdeaProjects\\e-lib(JavaEE)\\src\\main\\resources\\db.properties";
     private static volatile DataSource dataSource;
+    private final static Logger log = LogManager.getLogger(ConnectionPoolHolder.class);
 
     public static DataSource getDataSource() {
 
         if (dataSource == null) {
             synchronized (ConnectionPoolHolder.class) {
                 if (dataSource == null) {
-                    BasicDataSource ds = new BasicDataSource();
-                    ds.setUrl("jdbc:postgresql://localhost:5432/servlet_project");
-                    ds.setUsername("postgres");
-                    ds.setPassword("123454321");
-                    ds.setMinIdle(5);
-                    ds.setMaxIdle(10);
-                    ds.setMaxOpenPreparedStatements(100);
-                    dataSource = ds;
+                    try (FileReader reader = new FileReader(DB_PROPERTIES)) {
+                        Properties p = new Properties();
+                        p.load(reader);
+                        BasicDataSource ds = new BasicDataSource();
+                        ds.setUrl(p.getProperty("db_url"));
+                        ds.setUsername(p.getProperty("db_username"));
+                        ds.setPassword(p.getProperty("db_password"));
+                        ds.setMinIdle(Integer.parseInt(p.getProperty("db_min_idle")));
+                        ds.setMaxIdle(Integer.parseInt(p.getProperty("db_max_idle")));
+                        ds.setMaxOpenPreparedStatements(Integer.parseInt(
+                                p.getProperty("db_max_open_prepared_statement")));
+                        dataSource = ds;
+                    } catch (Exception e) {
+                        log.fatal("cannot connect to db - {}", e.getMessage());
+                        System.exit(-1);
+                    }
                 }
             }
         }
