e42b11bbbef2606dd8f4f3f8e9180c8442a424ee
KySo4oK <prostorostuk08@gmail.com>
2020-06-30 18:11:01 +0300

    implement exc handling

--- src/main/java/controller/filters/ExceptionHandlingFilter.java
+++ src/main/java/controller/filters/ExceptionHandlingFilter.java
@@ -1,19 +1,29 @@
 package controller.filters;
 
+import controller.util.LocaleExtractor;
+import controller.util.MessageSource;
 import model.exception.CustomException;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 public class ExceptionHandlingFilter implements Filter {
     public void destroy() {
     }
 
-    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
+            throws ServletException, IOException {
         try {
             chain.doFilter(req, resp);
         } catch (CustomException e) {
+            throw new RuntimeException(
+                    MessageSource.getMessage(e.getMessage(),
+                            LocaleExtractor.extractFromRequest((HttpServletRequest) req)));
         } catch (RuntimeException e) {
+            throw new RuntimeException(
+                    MessageSource.getMessage("error.unknown",
+                            LocaleExtractor.extractFromRequest((HttpServletRequest) req)));
         }
     }
 
